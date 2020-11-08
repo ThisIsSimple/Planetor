434d17aacf35fe5bfb111a7d942c4e593c40382b
JuniorEinstein <cordelia273@naver.com>
2020-11-08 13:08:38 +0900

    2020-11-08

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import cocos
 from cocos import draw
+import cocos.collision_model as cm
+from cocos.euclid import Vector2
 
 import pyglet
 from pyglet import shapes
 
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from collections import defaultdict
+from pyglet.window import key
 
 
 def print_hi(name):
@@ -19,21 +19,53 @@ class MainLayer(cocos.layer.Layer):
     def __init__(self):
         super(MainLayer, self).__init__()
 
-        planet = Planet(10)
+        line = draw.Line((0, 0), (550, 450), (255, 255, 255, 255))
+        self.add(line)
+
+        # circle = draw.Circle(Vector2(300, 300), 10.0)
+        # self.add(circle)
+
+        player = Player()
+        self.add(player)
+
+        planet = Planet((10, 10))
         self.add(planet)
 
+        self.schedule(self.update)
 
-class Planet(cocos.sprite.Sprite):
-    def __init__(self, radius):
-        batch = pyglet.graphics.Batch()
-        shape = shapes.Circle(300, 150, 100, color=(50, 225, 30), batch=batch)
-        super(Planet, self).__init__(shape)
+    def update(self, dt):
+        pass
+
+
+class Actor(cocos.sprite.Sprite):
+    def __init__(self, image, position, color=None):
+        if color:
+            super(Actor, self).__init__(image, position, color=color)
+        else:
+            super(Actor, self).__init__(image, position)
+
+        self.position = position
+        cm.CircleShape(self.position, self.width * 0.5)
+
+
+class Planet(Actor):
+    def __init__(self, position, color=None):
+        if color:
+            super(Planet, self).__init__("assets/planet.png", position=position, color=color)
+        else:
+            super(Planet, self).__init__("assets/planet.png", position=position)
+
+
+class Player(Actor):
+    def __init__(self):
+        super(Player, self).__init__("assets/planet.png", position=(100, 100))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     cocos.director.director.init(caption='Planetor')
-    scene = cocos.scene.Scene()
+    mainLayer = MainLayer()
+    scene = cocos.scene.Scene(mainLayer)
     cocos.director.director.run(scene)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
